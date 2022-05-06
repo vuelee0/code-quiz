5f40806b205ad47957926719fe09730421f38757
Vue Lee <vuelee09@gmail.com>
2022-05-05 23:39:12 -0500

    html and css setup, add readme.md

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+var buttonEl = document.querySelector("#btn");
+var highScoresEl = document.querySelector("#scores");
+var timerEl = document.querySelector("#timer");
+var startEl = document.querySelector(".start");
+
+
+buttonEl.addEventListener("click", function() {
+    startEl.style.display = "none"
+})
